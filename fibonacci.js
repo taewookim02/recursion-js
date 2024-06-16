3f639dc44012987d9ee84d4ad5ce02c9e8238be9
Tae Woo Kim <taewookim02@gmail.com>
2024-06-16 14:36:58 +0900

    Add optimized fib

--- fibonacci.js
+++ fibonacci.js
@@ -3,8 +3,18 @@ document.addEventListener("DOMContentLoaded", (e) => {
     if (n === 0) return 0;
     if (n === 1) return 1;
 
-    // need to get previous num
     return fib(n - 1) + fib(n - 2);
+  };
+
+  const optimizedFib = (n) => {
+    let a = 1;
+    let b = 1;
+    for (let i = 3; i <= n; i++) {
+      let c = a + b;
+      a = b;
+      b = c;
+    }
+    return b;
   };
 
   const form = document.querySelector(".fibonacci-form");
@@ -13,7 +23,8 @@ document.addEventListener("DOMContentLoaded", (e) => {
     const val = +form.querySelector(".fibonacci-input").value;
     const resultTag = form.querySelector(".fibonacci-result");
     try {
-      const result = fib(val);
+      // const result = fib(val);
+      const result = optimizedFib(val);
       resultTag.innerHTML = `result: ${result}`;
     } catch (error) {
       resultTag.innerHTML = `error: ${error}`;
